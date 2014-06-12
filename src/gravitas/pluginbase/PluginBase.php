bec708c0f6704c1c1cdae40d3ef1b1ac9a080386
Tyler Lartonoix <larttyler@live.com>
2014-06-12 17:20:32 -0400

    More wordpress fixes

--- src/gravitas/pluginbase/PluginBase.php
+++ src/gravitas/pluginbase/PluginBase.php
@@ -1,27 +1,24 @@
 <?php
 	namespace gravitas\pluginbase;
 
-	use \add_menu_page;
-	use \add_action;
-
 	abstract class PluginBase {
 		public function addMenuPage($title, $type, $plug, $pageName) {
 			$main = $this;
 
-			add_menu_page($title, $title, $type, $plug, function() use ($main, $pageName) {
+			\add_menu_page($title, $title, $type, $plug, function() use ($main, $pageName) {
 				$main->getPage($pageName);
 			});
 		}
 
 		public function getPage($page) {
-			if (!file_exists(sprintf('%spages/%s.php', plugin_dir_path(__FILE__), $page)))
+			if (!file_exists(sprintf('%spages/%s.php', \plugin_dir_path(__FILE__), $page)))
 				echo '<div><strong>Page not found.</strong></div>';
 			else
-				echo file_get_contents(plugins_url('pages/' . $page . '.php', __FILE__));
+				echo file_get_contents(\plugins_url('pages/' . $page . '.php', __FILE__));
 		}
 
 		public function addNotice(NoticeLevel $level, $message) {
-			add_action('admin_notices', function() use ($level, $message) {
+			\add_action('admin_notices', function() use ($level, $message) {
 				printf('<div class="%1$s"><p>%2$s</p></div>', $level, $message);
 			});
 		}
