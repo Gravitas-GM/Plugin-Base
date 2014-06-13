6f1541d01fd534be58892af04d3ff2414756c881
Tyler Lartonoix <larttyler@live.com>
2014-06-13 14:50:35 -0400

    Fixed directory finding issues caused by Wordpresses default plugin functions

--- src/gravitas/pluginbase/PluginBase.php
+++ src/gravitas/pluginbase/PluginBase.php
@@ -2,6 +2,8 @@
 	namespace gravitas\pluginbase;
 
 	abstract class PluginBase {
+		const PLUGIN_ROOT_SEARCH_LEN = 18;
+
 		public function addMenuPage($title, $pageName, $pageType = null, $hook = 'admin_menu', $plug = null) {
 			if ($pageType === null)
 				$pageType = PageType::OPTIONS();
@@ -22,17 +24,32 @@
 		}
 
 		public function getPage($page) {
-			if (!file_exists(sprintf('%spages/%s.php', plugin_dir_path(__FILE__), $page)))
+			if (isset($_GET['debug']))
+				printf('%s/pages/%s.php', self::getPluginRoot(), $page);
+
+			if (!file_exists(sprintf('%s/pages/%s.php', self::getPluginRoot(), $page)))
 				return '<div><strong>Page not found.</strong></div>';
 
-
-			return file_get_contents(plugins_url('pages/' . $page . '.php', __FILE__));
+			return file_get_contents(sprintf('%s/pages/%s.php', self::getPluginUrlRoot(), $page));
 		}
 
 		public function addNotice(NoticeLevel $level, $message) {
 			add_action('admin_notices', function() use ($level, $message) {
 				printf('<div class="%1$s"><p>%2$s</p></div>', $level, $message);
 			});
+		}
+
+		public function getPluginRoot() {
+			$pos = strpos(__DIR__, "wp-content/plugins") + self::PLUGIN_ROOT_SEARCH_LEN;
+			
+			return substr(__DIR__, 0, strpos(__DIR__, '/', $pos + 1));
+		}
+
+		public function getPluginUrlRoot() {
+			$url = plugins_url('', __DIR__);
+			$pos = strpos($url, "wp-content/plugins") + self::PLUGIN_ROOT_SEARCH_LEN;
+
+			return substr($url, 0, strpos($url, '/', $pos + 1));
 		}
 	}
 ?>
